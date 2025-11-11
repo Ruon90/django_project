0429fba639bc5278ba2e64078efc887515e3273c
Ruon90 <143106386+Ruon90@users.noreply.github.com>
2025-11-11 15:18:52 +0000

    Enable debug

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i0%6d_=-s!)i@9aud+pdoa)@lu(0y6ij75k6^$rc-zwy41z6bv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
